dad07bce8fd07e4bbe7e1c5a1e37509f0842bad3
Brian Kan <BrianKan00@hotmail.com>
2019-11-25 18:13:39 -0500

    API for keyword and citation seems to be working.  Styling is next.

--- app/index.js
+++ app/index.js
@@ -1,14 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
+// import './index.css';
+import App from './App';
 import CiteIcon from "./icons/cite.svg"
-
-const App = () => (
-  <div>
-    Hello World with one method of svg usage <img src={CiteIcon} />
-  </div>
-
-  
-)
 
 
 ReactDOM.render(<App />, document.getElementById("app"))
